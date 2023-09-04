c601214aec62f63de8e1967be78bc8ce3618c36c
VeronicaCastro <veronicacastromurillo@gmail.com>
2023-09-03 21:45:50 -0600

    new mvc files uploaded

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Assignment_of_projects_and_tasks.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}

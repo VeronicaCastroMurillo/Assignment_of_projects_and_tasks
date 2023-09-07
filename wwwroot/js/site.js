9350a33ac82ca0a4faf3e9b9d34413c11fc64604
VeronicaCastro <veronicacastromurillo@gmail.com>
2023-09-07 16:47:54 -0600

    all views completed successfully

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -30,3 +30,37 @@ var waveBtn = (function () {
         };
     }
 }());
+
+// Barra lateral
+function openSideNav() {
+    const btnOpenSideNav = document.getElementById("BtnOpenSideNav");
+
+    if (btnOpenSideNav.getAttribute("data-value") === "false") {
+        sidemenu.style.marginLeft = "250px";
+        btnOpenSideNav.setAttribute("data-value", "true");
+
+        // Establecer la posición fija y la altura máxima para el desplazamiento
+        sidemenu.style.position = "fixed";
+        
+    } else {
+        sidemenu.style.marginLeft = "0px";
+        btnOpenSideNav.setAttribute("data-value", "false");
+
+        // Restaurar el estilo del sidemenu y permitir el desplazamiento del body
+        sidemenu.style.position = "";
+        sidemenu.style.maxHeight = "";
+    }
+}
+
+// Cerrar barra lateral si se toca fuera del area
+document.onclick = function (e) {
+
+    // Si el usuario hace click fuera del algunos de esos marcos
+    if (e.target.id !== 'sidemenu' &&
+        e.target.id !== 'topBar' && 
+        e.target.id !== 'BtnOpenSideNav'
+    ) {
+        document.getElementById("sidemenu").style.marginLeft = "0px";;
+        document.getElementById("BtnOpenSideNav").setAttribute("data-value", "false");
+    }
+}
